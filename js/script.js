ebc4b7efad58e8f67b8678fd029b069712ca6db5
JpabloMF <juanpablo45671@gmail.com>
2019-11-11 15:36:16 -0500

    adding projects section

--- js/script.js
+++ js/script.js
@@ -1,8 +1,38 @@
 (function() {
-  const el = document.querySelector("main");
+  AOS.init();
+  
+  function moveBackground() {
+    const element = document.querySelector('body');
 
-  el.addEventListener("mousemove", e => {
-    el.style.backgroundPositionX = -e.offsetX + "px";
-    el.style.backgroundPositionY = -e.offsetY + "px";
-  });
+    element.addEventListener('mousemove', (e) => {
+      element.style.backgroundPositionX = -e.offsetX + 'px';
+      element.style.backgroundPositionY = -e.offsetY + 'px';
+    });
+  }
+  
+  function selectItemsMenu(){
+    const elements = document.querySelectorAll('#menu li');
+    const selectedElement = "";
+  }
+
+  var lastScrollTop = 0;
+  // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+  document.querySelector('body').addEventListener(
+    'scroll',
+    function() {
+      // or window.addEventListener("scroll"....
+      var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+      if (st > lastScrollTop) {
+        // downscroll code
+        console.log('downscroll');
+      } else {
+        // upscroll code
+        console.log('upscroll');
+      }
+      lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+    },
+    false
+  );
+
+  moveBackground();
 })();
